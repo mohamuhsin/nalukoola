a09eb984991a99825d65ef363070f636cb5809bb
mohamuhsin <muhsinohoho@outlook.com>
2025-02-17 20:38:03 +0300

    added changes

--- components/Header/MobileMenu.js
+++ components/Header/MobileMenu.js
@@ -16,7 +16,7 @@ export default function MobileMenu({ toggleMenu }) {
                 {/* Home Link with Icon */}
                 <Link
                     href="/"
-                    className="flex items-center gap-3 text-lg sm:text-xl text-white hover:text-white transition duration-300 ease-in-out w-full py-2 px-3 rounded-lg"
+                    className="flex items-center gap-3 text-lg sm:text-xl text-gray-300 hover:text-white transition duration-300 ease-in-out w-full py-2 px-3 rounded-lg"
                     aria-label="Home"
                 >
                     <FiHome className="text-3xl text-[#dd0300]" />
@@ -26,7 +26,7 @@ export default function MobileMenu({ toggleMenu }) {
                 {/* Phone Number Link */}
                 <Link
                     href="tel:+256700123456"
-                    className="flex items-center gap-3 text-lg sm:text-xl text-white hover:text-white transition duration-300 ease-in-out w-full py-2 px-3 rounded-lg"
+                    className="flex items-center gap-3 text-lg sm:text-xl text-gray-300 hover:text-white transition duration-300 ease-in-out w-full py-2 px-3 rounded-lg"
                     aria-label="Phone Number"
                 >
                     <FiPhone className="text-3xl text-[#dd0300]" />
@@ -36,7 +36,7 @@ export default function MobileMenu({ toggleMenu }) {
                 {/* Email Link */}
                 <Link
                     href="mailto:nalukoola@example.com"
-                    className="flex items-center gap-3 text-lg sm:text-xl text-white hover:text-white transition duration-300 ease-in-out w-full py-2 px-3 rounded-lg"
+                    className="flex items-center gap-3 text-lg sm:text-xl text-gray-300 hover:text-white transition duration-300 ease-in-out w-full py-2 px-3 rounded-lg"
                     aria-label="Email"
                 >
                     <FiMail className="text-3xl text-[#dd0300]" />
