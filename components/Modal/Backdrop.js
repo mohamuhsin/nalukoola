8155e78de9683ffcbe9385830e4a84aca6975a82
mohamuhsin <muhsinohoho@outlook.com>
2025-02-23 04:48:44 +0300

    added payment modal

--- components/Modal/Backdrop.js
+++ components/Modal/Backdrop.js
@@ -3,29 +3,36 @@ import { useEffect, useRef } from "react";
 export default function Backdrop({ onClose }) {
     const backdropRef = useRef(null);
 
+    // Handle pressing Escape to close the modal
     const handleKeyDown = (e) => {
         if (e.key === "Escape") {
             onClose();
         }
     };
 
+    // Handle clicking outside the modal to close the backdrop
     const handleCloseBackdrop = (e) => {
         if (e.target === backdropRef.current) {
             onClose();
         }
     };
 
+    // Use the effect to handle keydown event and cleanup
     useEffect(() => {
         backdropRef.current?.focus();
+
+        return () => {
+            // Cleanup if necessary
+        };
     }, []);
 
     return (
         <div
             ref={backdropRef}
-            className="fixed inset-0 bg-black bg-opacity-70 z-[9999] flex items-center justify-center"
+            className="fixed inset-0 bg-black bg-opacity-70 z-[9999] flex items-center justify-center" // Reduced opacity
             onClick={handleCloseBackdrop}
             onKeyDown={handleKeyDown}
-            tabIndex={-1}
+            tabIndex={0}
         />
     );
 }
