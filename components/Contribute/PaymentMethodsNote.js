2be120611b44abf744a47c3cf807364df5a38c27
mohamuhsin <muhsinohoho@outlook.com>
2025-02-23 05:06:52 +0300

    updated payment note

--- components/Contribute/PaymentMethodsNote.js
+++ components/Contribute/PaymentMethodsNote.js
@@ -1,7 +1,7 @@
 const PaymentMethodsNote = () => (
     <p className="mt-6 text-base md:text-lg text-gray-700 font-semibold bg-yellow-100 p-4 rounded-lg shadow-lg">
-        Use the payment numbers below to contribute via Airtel Money, MTN Mobile
-        Money, or Bank of Africa.
+        To get your receipt, use the account numbers below to contribute via Airtel
+        Money, MTN Mobile Money, or Bank of Africa.
     </p>
 );
 
